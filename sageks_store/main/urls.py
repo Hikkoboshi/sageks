90845d17eeefe7e116883417186a1efc998390ef
Aleksey <hikkoboshisatori@gmail.com>
2022-07-29 16:41:09 +0300

    [upd]

--- sageks_store/main/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.homePage, name='homePage'),
-    path('catalog/', views.catalogPage, name='catalogPage'),
-    path('catalog/<int:pk>/', views.productPage, name='productPage'),
-]
